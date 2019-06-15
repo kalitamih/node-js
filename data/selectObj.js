725f3781e821b7ecfe898bb6264023076afa07f1
Пользователь <mkalita1991@gmail.com>
2019-06-15 03:10:53 +0300

    refactor: change select controller

--- data/selectObj.js
+++ data/selectObj.js
@@ -1,19 +1,10 @@
-const nameSet = new Set();
-const birthYearSet = new Set();
-const eyeColorSet = new Set();
-const genderSet = new Set();
-const hairColorSet = new Set();
-const heightSet = new Set();
-const massSet = new Set();
-const skinColorSet = new Set();
-
 module.exports.selectObj = {
-  name: nameSet,
-  birth_year: birthYearSet,
-  eye_color: eyeColorSet,
-  gender: genderSet,
-  hair_color: hairColorSet,
-  height: heightSet,
-  mass: massSet,
-  skin_color: skinColorSet,
+  name: new Set(['']),
+  birth_year: new Set(['']),
+  eye_color: new Set(['']),
+  gender: new Set(['']),
+  hair_color: new Set(['']),
+  height: new Set(['']),
+  mass: new Set(['']),
+  skin_color: new Set(['']),
 };
